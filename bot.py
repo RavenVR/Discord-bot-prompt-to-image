415479c07390672dadc1f285856db84eaddc7a1c
RavenVR <100008567+RavenVR@users.noreply.github.com>
2023-06-24 22:26:08 +1000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import hikari, lightbulb, random, json, os, openai, requests
+from PIL import Image
+from io import BytesIO
+# Defining the bot
+bot = lightbulb.BotApp(token='BOT TOKEN', default_enabled_guilds=GUILD ID, intents=hikari.Intents.ALL)
+
+# OpenAI token
+openai.api_key = 'OPEN AI KEY'
+
+
+@bot.command
+@lightbulb.option("prompt", "The prompt for the AI")
+@lightbulb.command("ai-txt-to-image", "Text to image")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def aiimggenerator(ctx):
+    PROMPT = str(ctx.options.prompt)
+    await ctx.respond("loading...")
+    
+    response = openai.Image.create(
+        prompt=PROMPT,
+        n=1,
+        size="256x256",
+    )
+    response = requests.get(response['data'][0]['url'])
+    await ctx.edit_last_response(response.content)
+
+# Start the bot
+bot.run(
+    status=hikari.Status.ONLINE,
+    activity=hikari.Activity(
+        name=f"random number: {random.randint(1,100)}",
+        type=hikari.ActivityType.WATCHING,
+    ),
+)
